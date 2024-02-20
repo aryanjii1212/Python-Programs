0d539ec289d5227a5347f5717e6af6f6be7cef66
aryanjii1212 <160573128+aryanjii1212@users.noreply.github.com>
2024-02-20 11:55:07 +0530

    Add files via upload

---
+++ star 1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+n=5
+for i in range(0, 5):
+    for j in range(0,i+1):
+        print("* ",end="")
+    print(" ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
